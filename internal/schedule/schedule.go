f8d2cc762ecb2f9ed4d0d22576e23663bd44e771
Jonas Tranberg <jonastranberg93@gmail.com>
2022-04-27 21:26:55 +0200

    change consume to be like watermill subscribe

--- internal/schedule/schedule.go
+++ internal/schedule/schedule.go
@@ -8,14 +8,17 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-type ConsumerFunc func(id string, data interface{})
+type Event struct {
+	id   string
+	data interface{}
+}
 
 type Schedule interface {
 	Add(ctx context.Context, cron *cron.Schedule, data interface{}) (id string, err error)
 	Set(ctx context.Context, id string, data interface{}) (err error)
 	Remove(ctx context.Context, id string) (err error)
 	Run(ctx context.Context, id string) (err error)
-	Consume(ctx context.Context, f ConsumerFunc) (err error)
+	Consume(ctx context.Context) (events <-chan *Event, err error)
 }
 
 type RedisSchedule struct {
@@ -42,6 +45,6 @@ func (rs *RedisSchedule) Run(ctx context.Context, id string) (err error) {
 	return errors.New("not implemented")
 }
 
-func (rs *RedisSchedule) Consume(ctx context.Context, f ConsumerFunc) (err error) {
-	return errors.New("not implemented")
+func (rs *RedisSchedule) Consume(ctx context.Context) (events <-chan *Event, err error) {
+	return nil, errors.New("not implemented")
 }
