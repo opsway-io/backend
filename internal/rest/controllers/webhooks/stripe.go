a81fd80dc147790ec1ed55de4a73b84a5367a193
teisnp <teis.n.p@gmail.com>
2024-02-01 19:06:40 +0100

    dereft checkout session

--- internal/rest/controllers/webhooks/stripe.go
+++ internal/rest/controllers/webhooks/stripe.go
@@ -50,8 +50,9 @@ func (h *Handlers) handleWebhook(c hs.StripeContext) error {
 		c.Log.Error(session.ClientReferenceID)
 		c.Log.Error(session)
 		c.Log.Error(session.ID)
+		c.Log.Error(*sessionWithLineItems)
 		lineItems := sessionWithLineItems.LineItems
-		c.Log.Error(*lineItems)
+		c.Log.Error(lineItems)
 
 		teamID, err := strconv.ParseUint(session.ClientReferenceID, 10, 32)
 		if err != nil {
