0864531cbe16ed59f4528d3fa37ad248bd33efe9
teisnp <teis.n.p@gmail.com>
2026-09-07 22:27:04 +0200

    fix: tag only unresolved incidents on monitor grid

--- internal/monitor/repository.go
+++ internal/monitor/repository.go
@@ -102,7 +102,7 @@ func (r *RepositoryImpl) GetMonitorsAndIncidentsByTeamID(ctx context.Context, te
 	var monitors []entities.Monitor
 	err := r.db.WithContext(
 		ctx,
-	).Preload("Incidents", "resolved = ? OR acknowledged = ?", false, false).
+	).Preload("Incidents", "resolved = ?", false).
 		Where(entities.Monitor{
 			TeamID: teamID,
 		}).Find(&monitors).Error
